b82d261f20359d955ea40a15a421c2ac1fb07466
Raihan Rafid <raihanrfd1@gmail.com>
2021-05-09 11:13:40 +0700

    add functions

--- functions/supermario.js
+++ functions/supermario.js
@@ -1,4 +1,10 @@
 exports.handler = async () => {
-  
-};
+  console.log("function ran");
 
+  const data = { name: "mario", age: 35, job: "Kuli" };
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data),
+  };
+};
