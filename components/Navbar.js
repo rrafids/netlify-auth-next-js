0b457635493e75c90018ad8616dbb0c70fff82fb
Raihan Rafid <raihanrfd1@gmail.com>
2021-05-09 10:59:51 +0700

    update navbar auth ready

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,36 +4,38 @@ import { useContext } from "react";
 import AuthContext from "../stores/authContext";
 
 export default function Navbar() {
-  const { user, login, logout } = useContext(AuthContext);
+  const { user, login, logout, authReady } = useContext(AuthContext);
 
   return (
     <div className="container">
       <nav>
         <Image src="/rupee.png" width={50} height={48} />
         <h1>Gaming Vibes</h1>
-        <ul>
-          <li>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/guides">
-              <a>Guides</a>
-            </Link>
-          </li>
-          {!user && (
-            <li onClick={login} className="btn">
-              Login / Sign up
+        {authReady && (
+          <ul>
+            <li>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
             </li>
-          )}
-          {user && <li>{user.email}</li>}
-          {user && (
-            <li onClick={logout} className="btn-red">
-              Log Out
+            <li>
+              <Link href="/guides">
+                <a>Guides</a>
+              </Link>
             </li>
-          )}
-        </ul>
+            {!user && (
+              <li onClick={login} className="btn">
+                Login / Sign up
+              </li>
+            )}
+            {user && <li>{user.email}</li>}
+            {user && (
+              <li onClick={logout} className="btn-red">
+                Log Out
+              </li>
+            )}
+          </ul>
+        )}
       </nav>
       <div className="banner">
         <Image src="/banner.png" width={966} height={276} />
--- stores/authContext.js
+++ stores/authContext.js
@@ -10,6 +10,7 @@ const AuthContext = createContext({
 
 export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [authReady, setAuthReady] = useState(false);
 
   useEffect(() => {
     netlifyIdentity.on("login", (user) => {
@@ -22,6 +23,12 @@ export const AuthContextProvider = ({ children }) => {
     netlifyIdentity.on("logout", () => {
       setUser(null);
       console.log("logout event");
+    });
+
+    netlifyIdentity.on("init", () => {
+      setUser(user);
+      setAuthReady(true);
+      console.log("init event");
     });
 
     // Init netlify identity connection
@@ -41,7 +48,7 @@ export const AuthContextProvider = ({ children }) => {
     netlifyIdentity.logout();
   };
 
-  const context = { user, login, logout };
+  const context = { user, login, logout, authReady };
 
   return (
     <AuthContext.Provider value={context}>{children}</AuthContext.Provider>
